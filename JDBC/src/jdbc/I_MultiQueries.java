701b5fe4423b1cf54293ad26a9c1cad7d9dc3767
BRNTZN <BRNTZN@192.168.0.135>
2015-10-04 19:22:53 +0200

    MultiQueries

---
+++ JDBC/src/jdbc/I_MultiQueries.java
@@ -0,0 +1,81 @@
+package jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class I_MultiQueries {
+
+	public static void main(String[] args) {
+		Connection conn;
+		try {
+			conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/databank?allowMultiQueries=true", "root",
+					"root");
+			try {
+				Statement s = conn.createStatement();
+				boolean isResultSet = s.execute("select * from tabel;"
+						+ "update tabel set voornaam = 'Lex' where voornaam = 'Vince';" + "select * from tabel;");
+				int updateCount = s.getUpdateCount();
+				while (isResultSet || updateCount!=-1) {
+					/**
+					 * if query is select => isResultset
+					 * if query is update => updatecount != -1
+					 */
+					if (isResultSet) {
+						ResultSet rs = s.getResultSet();
+						while (rs.next()) {
+							System.out.println(rs.getString(1)+rs.getString(2)+rs.getString(3));
+						}
+					}
+					isResultSet = s.getMoreResults();
+					updateCount = s.getUpdateCount();
+				}
+			} finally {
+				conn.close();
+			}
+		} catch (SQLException e1) {
+			e1.printStackTrace();
+		}
+		
+		withPrepared();
+	}
+	
+	/**
+	 * Kan ook met prepared statements
+	 */
+	private static void withPrepared(){
+		Connection conn;
+		try {
+			conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/databank?allowMultiQueries=true", "root",
+					"root");
+			try {
+				PreparedStatement ps = conn.prepareStatement("select * from tabel;"
+						+ "update tabel set voornaam = 'Vince' where voornaam = 'Lex';" + "select * from tabel;");
+				boolean isResultSet = ps.execute();
+				int updateCount = ps.getUpdateCount();
+				while (isResultSet || updateCount!=-1) {
+					/**
+					 * if query is select => isResultset
+					 * if query is update => updatecount != -1
+					 */
+					if (isResultSet) {
+						ResultSet rs = ps.getResultSet();
+						while (rs.next()) {
+							System.out.println(rs.getString(1)+rs.getString(2)+rs.getString(3));
+						}
+					}
+					isResultSet = ps.getMoreResults();
+					updateCount = ps.getUpdateCount();
+				}
+			} finally {
+				conn.close();
+			}
+		} catch (SQLException e1) {
+			e1.printStackTrace();
+		}
+	}
+
+}
